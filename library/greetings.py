f3702fd01e8487166ecbc046342b5835fd1b7bac
PrasadTelasula <prasad.telasula@yahoo.com>
2020-03-29 23:59:42 +0530

    Update greetings.py

--- library/greetings.py
+++ library/greetings.py
@@ -7,18 +7,14 @@ def run_module():
         name=dict(type='str', required=True)
     )
 
-    # seed the result dict in the object
-    # we primarily care about changed and state
-    # change is if this module effectively modified the target
-    # state will include any data that you want your module to pass back
-    # for consumption, for example, in a subsequent task
+    
     module = AnsibleModule(
         argument_spec=module_args,
         supports_check_mode=True
     )
 
 
-    username = module.params['name']
+    
     result = dict(
         changed=False,
         original_message='Hello ' + module.params['name'] + ' Welcome to Ansible custom Module Demo.',
